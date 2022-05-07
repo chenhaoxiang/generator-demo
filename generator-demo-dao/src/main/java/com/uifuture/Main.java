12beab50a388ce1518f2a832265b497c8f5edb12
chenhaoxiang <uifuture@uifuture.com>
2022-05-07 11:36:32 +0800

    Default Changelist

--- generator-demo-dao/src/main/java/com/uifuture/Main.java
+++ generator-demo-dao/src/main/java/com/uifuture/Main.java
@@ -5,6 +5,7 @@
 package com.uifuture;
 
 import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.api.ShellRunner;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
@@ -19,20 +20,20 @@ import java.util.List;
  */
 public class Main {
     //运行方法一
-//    public static void main(String[] args) {
-//        args = new String[] { "-configfile", "D:\\github\\generator-demo\\generator-demo-dao\\src\\main\\resources\\generatorConfig.xml", "-overwrite" };
-//        ShellRunner.main(args);
-//    }
+    public static void main(String[] args) {
+        args = new String[] { "-configfile", "/Users/chenhx/Desktop/github/generator-demo/generator-demo-dao/src/main/resources/generatorConfig.xml", "-overwrite" };
+        ShellRunner.main(args);
+    }
 
     //运行方法二
-    public static void main(String[] args) throws Exception {
-        File configFile = new File("generator-demo-dao\\src\\main\\resources\\generatorConfig.xml");
-        List<String> warnings = new ArrayList<>();
-        ConfigurationParser cp = new ConfigurationParser(warnings);
-        Configuration config = cp.parseConfiguration(configFile);
-        DefaultShellCallback callback = new DefaultShellCallback(true);
-        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-        myBatisGenerator.generate(null);
-    }
+//    public static void main(String[] args) throws Exception {
+//        File configFile = new File("/Users/chenhx/Desktop/github/generator-demo/generator-demo-dao/src/main/resources/generatorConfig.xml");
+//        List<String> warnings = new ArrayList<String>();
+//        ConfigurationParser cp = new ConfigurationParser(warnings);
+//        Configuration config = cp.parseConfiguration(configFile);
+//        DefaultShellCallback callback = new DefaultShellCallback(true);
+//        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+//        myBatisGenerator.generate(null);
+//    }
 
 }
